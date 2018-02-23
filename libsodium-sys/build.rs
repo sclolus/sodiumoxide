276a40a82beece1389f0550bec5353292d5ddebf
miniske <miniske@Debian-Seb-PC.Debian-Seb-PC>
2018-02-23 16:19:07 +0100

    cfg(target_os) just didn't work

--- libsodium-sys/build.rs
+++ libsodium-sys/build.rs
@@ -3,12 +3,6 @@ extern crate pkg_config;
 use std::process::Command;
 
 
-#[cfg(target_os = "android")]
-const INSTALL_SCRIPT: &str = "build_scripts/libsodium-android.sh";
-
-#[cfg(not(target_os = "android"))]
-const INSTALL_SCRIPT: &str = "build_scripts/libsodium-linux.sh";
-
 fn main() {
 
     println!("cargo:rerun-if-env-changed=SODIUM_LIB_DIR");
@@ -16,16 +10,27 @@ fn main() {
     if let Ok(lib_dir) = env::var("SODIUM_LIB_DIR") {
         println!("cargo:rustc-link-search=native={}", lib_dir);
 
-        let mode = match env::var_os("SODIUM_STATIC") {
-            Some(_) => "static",
-            None => "dylib",
+        let mode = match env::var("SODIUM_STATIC") {
+            Ok(_) => "static",
+            Err(_) => "dylib",
         };
         println!("cargo:rustc-link-lib={0}=sodium", mode);
 
     } else {
-        eprintln!("Running: {}", INSTALL_SCRIPT);
         let pwd = env::var("OUT_DIR").unwrap();
-        let output = Command::new(INSTALL_SCRIPT)
+
+        let libsodium_path;
+        let install_script;
+        if std::env::var("TARGET").unwrap() == "arm-linux-androideabi" {
+            install_script = "build_scripts/libsodium-android.sh";
+            libsodium_path = "libsodium-stable/libsodium-android-armv6";
+        } else {
+            install_script = "build_scripts/libsodium-linux.sh";
+            libsodium_path = "libsodium-stable/libsodium-linux";
+        }
+        eprintln!("Running: {}", install_script);
+
+        let output = Command::new(install_script)
             .arg(pwd.clone())
             .output()
             .expect("Failed to execute libsodium build script");
@@ -35,13 +40,7 @@ fn main() {
             pkg_config::find_library("libsodium").unwrap();
             return ;
         }
-        let libsodium_path;
-        #[cfg(target_os = "android")] {
-            libsodium_path = "libsodium-stable/libsodium-android-armv6";
-        }
-        #[cfg(not(target_os = "android"))] {
-            libsodium_path = "libsodium-stable/libsodium-linux";
-        }
+
         println!("cargo:rustc-link-search=native={}/{}/lib", pwd, libsodium_path);
         println!("cargo:rustc-env=SODIUM_LIB_DIR={}/{}/lib", pwd, libsodium_path);
         println!("cargo:rustc-env=SODIUM_INCLUDE_DIR={}/{}/include", pwd, libsodium_path);
